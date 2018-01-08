75bec435fad11c5e2cd0c9ce9175d3b01479b7ec
J C Monary-Ernsdorff <jason.ernsdorff@gmail.com>
2018-01-08 14:49:05 -0800

    Added Player Data

--- SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
@@ -1,7 +1,52 @@
-//: Playground - noun: a place where people can play
+/****** SOCCER COORDINATOR ********
+        by Jernsdorff
+***********************************/
 
-import UIKit
+//Dictionary for each player
+/*
+ Joe Smith    42    YES    Jim and Jan Smith
+ Jill Tanner    36    YES    Clara Tanner
+ Bill Bon    43    YES    Sara and Jenny Bon
+ Eva Gordon    45    NO    Wendy and Mike Gordon
+ Matt Gill    40    NO    Charles and Sylvia Gill
+ Kimmy Stein    41    NO    Bill and Hillary Stein
+ Sammy Adams    45    NO    Jeff Adams
+ Karl Saygan    42    YES    Heather Bledsoe
+ Suzane Greenberg    44    YES    Henrietta Dumas
+ Sal Dali    41    NO    Gala Dali
+ Joe Kavalier    39    NO    Sam and Elaine Kavalier
+ Ben Finkelstein    44    NO    Aaron and Jill Finkelstein
+ Diego Soto    41    YES    Robin and Sarika Soto
+ Chloe Alaska    47    NO    David and Jamie Alaska
+ Arnold Willis    43    NO    Claire Willis
+ Phillip Helm    44    YES    Thomas Helm and Eva Jones
+ Les Clay    42    YES    Wynonna Brown
+ Herschel Krustofski    45    YES    Hyman and Rachel Krustofski
+*/
 
-var str = "Hello, playground"
+let joeSmith: [String: Any] = ["name": "Joe Smith", "height": 42, "experienced": true, "guardian names": "Jim and Jan Smith"]
+let jillTanner: [String: Any] = ["name": "Jill Tanner", "height": 36, "experienced": true, "guardian names": "Clara Tanner"]
+let billBon: [String: Any] = ["name": "Bill Bon", "height": 43, "experienced": true, "guardian names": "Sara and Jenny Bon"]
+let evaGordon: [String: Any] = ["name": "Eva Gordon", "height": 45, "experienced": false, "guardian names": "Wendy and Mike Gordon"]
+let mattGill: [String: Any] = ["name": "Matt Gill", "height": 40, "experienced": false, "guardian names": "Charles and Sylvia Gill"]
+let kimmyStein: [String: Any] = ["name": "Kimmy Stein", "height": 41, "experienced": false, "guardian names": "Bill and Hillary Stein"]
+let sammyAdams: [String: Any] = ["name": "Sammy Adams", "height": 45, "experienced": false, "guardian names": "Jeff Adams"]
+let karlSaygan: [String: Any] = ["name": "Karl Saygan", "height": 42, "experienced": true, "guardian names": "Heather Bledsoe"]
+let suzaneGreenberg: [String: Any] = ["name": "Suzan Greenberg", "height": 44, "experienced": true, "guardian names": "Henrietta Dumas"]
+let salDali: [String: Any] = ["name": "Sal Dali", "height": 41, "experienced": false, "guardian names": "Gala Dali"]
+let joeKavalier: [String: Any] = ["name": "Joe Kavalier", "height": 39, "experienced": false, "guardian names": "Sam and Elaine Kavalier"]
+let benFinkelstein: [String: Any] = ["name": "Ben Finkelstein", "height": 44, "experienced": false, "guardian names": "Aaron and Jill Finkelstein"]
+let diegoSoto: [String: Any] = ["name": "Diego Soto", "height": 41, "experienced": true, "guardian names": "Robin and Sarika Soto"]
+let chloeAlaska: [String: Any] = ["name": "Choloe Alaska", "height": 47, "experienced": false, "guardian names": "David and Jamie Alaska"]
+let arnoldWillis: [String: Any] = ["name": "Arnold Willis", "height": 43, "experienced": false, "guardian names": "Claire Willis"]
+let phillipHelm: [String: Any] = ["name": "Phillip Helm", "height": 44, "experienced": true, "guardian names": "Thomas Helm and Eva Jones"]
+let lesClay: [String: Any] = ["name": "Les Clay", "height": 42, "experienced": true, "guardian names": "Wynonna Brown"]
+let herschelKrustofski: [String: Any] = ["name": "Herschel Krustofski", "height": 45, "experienced": true, "guardian names": "Hyman and Rachel Krustofski"]
 
-//Test - does this upload.
+
+
+
+
+
+
+
